c7c443931c638a7ff871d3f2c0250f543e8e5344
Efriandika Pratama <efriandika@gmail.com>
2024-12-03 23:20:41 +0000

    Added: Day 3 Solution

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -11,24 +11,13 @@ fun main() {
 
 fun part1(inputLines: List<String>): Int {
     val regex = Regex("mul\\((\\d+),(\\d+)\\)")
-    var result = 0;
 
-    inputLines.forEach { input ->
-        val matches = regex.findAll(input)
-
-        for (match in matches) {
-            val x = match.groupValues[1].toInt();
-            val y = match.groupValues[2].toInt();
-
-            result += x * y;
-        }
-    }
-
-    return result;
+    return inputLines.sumOf { input -> regex.findAll(input).sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }}
 }
 
 fun part2(inputLines: List<String>): Int {
     val regex = Regex("(mul|don't|do)\\((\\d*),?(\\d*)\\)")
+
     var enabled = true;
     var result = 0;
 
@@ -41,7 +30,7 @@ fun part2(inputLines: List<String>): Int {
             when (operator) {
                 "do" -> enabled = true
                 "don't" -> enabled = false
-                else -> { // Note the block
+                else -> {
                     if (enabled) {
                         val x = match.groupValues[2].toInt()
                         val y = match.groupValues[3].toInt()
